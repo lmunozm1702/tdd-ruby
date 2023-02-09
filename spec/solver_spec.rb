b65d903fd25f4b5d3db0527ddf81af002169d168
Luis Muñoz <l.munoz.m@outlook.com>
2023-02-09 16:28:28 -0300

    Factorial 0 OK

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -5,7 +5,7 @@ describe Solver do
   describe '#factorial' do
     it 'returns 1 when the value is 0' do
       solver = Solver.new
-      expect(solver.factorial).to eql(0)
+      expect(solver.factorial(0)).to eql(1)
     end
   end
 end
