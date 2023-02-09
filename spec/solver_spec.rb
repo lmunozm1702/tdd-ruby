b300c44751709a381d50d39465d2c24ec376668d
Luis Muñoz <l.munoz.m@outlook.com>
2023-02-09 16:25:36 -0300

    Test factorial method doesn't exists

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -2,5 +2,10 @@ require 'rspec'
 require_relative '../solver'
 
 describe Solver do
-  
+  describe '#factorial' do
+    it 'returns 1 when the value is 0' do
+      solver = Solver.new
+      expect(solver.factorial).to eql(0)
+    end
+  end
 end
