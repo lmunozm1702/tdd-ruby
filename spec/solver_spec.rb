560a039bcfb47f386e764588af6ad80684ed2436
Abeera Tahir <abeeratahir2018@gmail.com>
2023-02-10 00:39:26 +0500

    Add test for negative value of factorial

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -2,14 +2,18 @@ require 'rspec'
 require_relative '../solver'
 
 describe Solver do
+  before :each do
+    @solver = Solver.new
+  end
   describe '#factorial' do
     it 'returns 1 when the value is 0' do
-      solver = Solver.new
-      expect(solver.factorial(0)).to eql(1)
+      expect(@solver.factorial(0)).to eql(1)
     end
     it 'returns factorial when the value is > 0' do
-      solver = Solver.new
-      expect(solver.factorial(5)).to eql(120)
+      expect(@solver.factorial(5)).to eql(120)
+    end
+    it 'raise exception when the value is < 0' do
+      expect{ @solver.factorial(-1) }.to raise_error(ArgumentError)
     end
   end
 end
