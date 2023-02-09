2f13188561d4901bcd912e2b1efeb936eb4321d9
Luis Muñoz <l.munoz.m@outlook.com>
2023-02-09 16:29:36 -0300

    factorial method ever returns 0

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -7,5 +7,9 @@ describe Solver do
       solver = Solver.new
       expect(solver.factorial(0)).to eql(1)
     end
+    it 'returns factorial when the value is > 0' do
+      solver = Solver.new
+      expect(solver.factorial(5)).to eql(120)
+    end
   end
 end
