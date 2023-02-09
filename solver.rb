2f80b7b73dfca97c61a5a4dc7dc93e6ea04fde9f
Abeera Tahir <abeeratahir2018@gmail.com>
2023-02-10 00:50:52 +0500

    Add reverse method

--- solver.rb
+++ solver.rb
@@ -3,4 +3,7 @@ class Solver
     raise ArgumentError.new("Expected a value of 1-#{number}, got #{number}") if number < 0
     (1..number).reduce(:*) || 1
   end
+  def reverse(string)
+    string.reverse
+  end
 end
