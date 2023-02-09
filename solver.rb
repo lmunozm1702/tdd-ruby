88e5a36ae623485f3d0ac1d96c39fc91990cea28
Abeera Tahir <abeeratahir2018@gmail.com>
2023-02-10 01:15:52 +0500

    Fix rubocop errors

--- solver.rb
+++ solver.rb
@@ -1,6 +1,9 @@
+# Class solver definition
+# frozen_string_literal: true
+
 class Solver
   def factorial(number)
-    raise ArgumentError, "Expected a value of 1-#{number}, got #{number}" if number < 0
+    raise ArgumentError, "Expected a value of 1-#{number}, got #{number}" if number.negative?
 
     (1..number).reduce(:*) || 1
   end
@@ -10,9 +13,9 @@ class Solver
   end
 
   def fizzbuzz(number)
-    return 'fizzbuzz' if number % 3 == 0 && number % 5 == 0
-    return 'fizz' if number % 3 == 0
-    return 'buzz' if number % 5 == 0
+    return 'fizzbuzz' if (number % 3).zero? && (number % 5).zero?
+    return 'fizz' if (number % 3).zero?
+    return 'buzz' if (number % 5).zero?
 
     number.to_s
   end
--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
+
+# rubocop:disable Metrics/BlockLength
+
 require 'rspec'
 require_relative '../solver'
 
@@ -6,34 +9,30 @@ describe Solver do
   before :each do
     @solver = Solver.new
   end
-  describe '#factorial' do
-    it 'returns 1 when the value is 0' do
-      expect(@solver.factorial(0)).to eql(1)
-    end
-    it 'returns factorial when the value is > 0' do
-      expect(@solver.factorial(5)).to eql(120)
-    end
-    it 'raise exception when the value is < 0' do
-      expect{ @solver.factorial(-1) }.to raise_error(ArgumentError)
-    end
+  it 'returns 1 when the value is 0' do
+    expect(@solver.factorial(0)).to eql(1)
   end
-  describe '#reverse' do
-    it 'returns "olleh" when the value is "hello"' do
-      expect(@solver.reverse('hello')).to eql('olleh')
-    end
+  it 'returns factorial when the value is > 0' do
+    expect(@solver.factorial(5)).to eql(120)
   end
-  describe '#fizzbuzz ' do
-    it 'returns "fizz" when the value is divisible by 3' do
-      expect(@solver.fizzbuzz(9)).to eql('fizz')
-    end
-    it 'returns "buzz" when the value is divisible by 5' do
-      expect(@solver.fizzbuzz(10)).to eql('buzz')
-    end
-    it 'returns "fizzbuzz" when the value is divisible by 3 and 5' do
-      expect(@solver.fizzbuzz(15)).to eql('fizzbuzz')
-    end
-    it 'returns the same value as string in any other case' do
-      expect(@solver.fizzbuzz(7)).to eql('7')
-    end
+  it 'raise exception when the value is < 0' do
+    expect { @solver.factorial(-1) }.to raise_error(ArgumentError)
+  end
+  it 'returns "olleh" when the value is "hello"' do
+    expect(@solver.reverse('hello')).to eql('olleh')
+  end
+  it 'returns "fizz" when the value is divisible by 3' do
+    expect(@solver.fizzbuzz(9)).to eql('fizz')
+  end
+  it 'returns "buzz" when the value is divisible by 5' do
+    expect(@solver.fizzbuzz(10)).to eql('buzz')
+  end
+  it 'returns "fizzbuzz" when the value is divisible by 3 and 5' do
+    expect(@solver.fizzbuzz(15)).to eql('fizzbuzz')
+  end
+  it 'returns the same value as string in any other case' do
+    expect(@solver.fizzbuzz(7)).to eql('7')
   end
 end
+
+# rubocop:enable Metrics/BlockLength
